2e6772388d1d73ddaf9308b5c6a2e87cc924f9e1
davealone69-gif <davealone69@gmail.com>
2026-09-01 01:42:12 +0800

    Export the GLB renderer integration surface

---
+++ src/renderer/gltf/index.ts
@@ -0,0 +1,6 @@
+export * from './GltfTypes';
+export * from './GlbLoader';
+export * from './GltfTexture';
+export * from './GltfMaterial';
+export * from './GltfMesh';
+export * from './GltfAvatar';

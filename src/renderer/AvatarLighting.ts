95833229b3322ad7572e937f769a059be582c110
davealone69-gif <davealone69@gmail.com>
2026-09-01 04:31:49 +0800

    Add three-point avatar lighting configuration

---
+++ src/renderer/AvatarLighting.ts
@@ -0,0 +1,2 @@
+export interface AvatarLighting { key:[number,number,number]; fill:[number,number,number]; rim:[number,number,number]; intensity:number; }
+export const DEFAULT_AVATAR_LIGHTING:AvatarLighting={key:[2.5,3.2,2.2],fill:[-2,1,1],rim:[0,2,-3],intensity:1};

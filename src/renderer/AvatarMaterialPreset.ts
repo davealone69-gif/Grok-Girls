03a6ca2272768f2187ead0a60dc2f370ab8b33f2
davealone69-gif <davealone69@gmail.com>
2026-09-01 04:31:55 +0800

    Add skin material presets

---
+++ src/renderer/AvatarMaterialPreset.ts
@@ -0,0 +1,3 @@
+export interface AvatarMaterialPreset { skinTone:[number,number,number]; roughness:number; subsurface:number; oil:number; metallic:number; }
+export const NATURAL_SKIN:AvatarMaterialPreset={skinTone:[.72,.48,.36],roughness:.48,subsurface:.32,oil:.12,metallic:0};
+export const PORCELAIN_SKIN:AvatarMaterialPreset={skinTone:[.92,.78,.72],roughness:.38,subsurface:.24,oil:.08,metallic:0};

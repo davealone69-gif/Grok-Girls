894ecf625869baa481e3cd53e1e6c544d6f40a87
davealone69-gif <davealone69@gmail.com>
2026-09-01 09:23:46 +0800

    Wire completed phone navigation menu

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { installRendererDiagnostics } from './renderer/RendererDiagnostics';
+import './menu-mobile.css';
 
 installRendererDiagnostics();
 
